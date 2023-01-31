0e0ba6cc25d9c440c15237956aa3119b36d7be16
Claus Kühnel <info@ckuehnel.ch>
2023-01-31 12:22:06 +0100

    File Upload

--- RAK3372_HW/RAK3372_HW.ino
+++ RAK3372_HW/RAK3372_HW.ino
@@ -2,13 +2,14 @@
  * File: RAK3372_HW.ino
  * 
  * Test of Core RAK3372 installed on Base RAK19001
+ * Connect IO1 and SW1 on RAP19001
  * 
  * 2023-01-25 Claus Kuehnel info@ckuehnel.ch
  */
 // set pin number for WISBLOCK_BASE_19001
 uint8_t ledGreenPin = GREEN_LED;  // set ledpin for Green Led
 uint8_t ledBluePin  = BLUE_LED;	  // set ledpin for Blue Led
-uint8_t inputPin    = WB_IO1;	    // set inputPin to IO1  CK: external connection between IO1 and SW1, SW1 vnot available on RAK3372
+uint8_t inputPin    = WB_IO1;	    // set inputPin to IO1  CK: external connection between IO1 and SW1, SW1 not available on RAK3372
 
 void setup()
 {
@@ -29,6 +30,7 @@ void loop()
   Serial.println("This is RAK3372 based on STM32WLE5CC");
 
   int val = digitalRead(inputPin);	// read input pin value (HIGH or LOW)
+ 
   if (val == LOW)
     digitalWrite(ledGreenPin, HIGH);	// LED turn on when input pin value is LOW
   else
---
+++ RAK3372_I2C_Test/RAK3372_I2C_Test.ino
@@ -0,0 +1,27 @@
+/*
+ * File: RAK3372_I2C_Test.ino
+ * Test I2C bus on RAK3372
+ * 
+ */
+#include <Wire.h>
+#include <i2cdetect.h>
+
+#define DEBUG 1
+
+void setup() 
+{
+  Serial.begin(115200);
+  delay(2000);              // wait for serial monitor
+  if (DEBUG) Serial.println(F("Initializing..."));
+  Serial.println("Test I2C Bus...");
+  Wire.begin();
+  Wire.setClock(400000);
+  if (DEBUG) Serial.println(F("Running...")); //last line in setup()
+}
+
+void loop() 
+{
+  Serial.println("\nScanning I2C Bus");
+  i2cdetect(1,127);
+  delay(5000);
+}
